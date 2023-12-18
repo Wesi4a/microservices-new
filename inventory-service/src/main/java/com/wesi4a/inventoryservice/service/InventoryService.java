2716f252c51ab6a5bd01896613a2972277370776
Veselin Kokoshkov <veselinak1510@gmail.com>
2023-12-18 17:06:34 +0200

    communication between inventory and order service

--- inventory-service/src/main/java/com/wesi4a/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/wesi4a/inventoryservice/service/InventoryService.java
@@ -1,9 +1,14 @@
 package com.wesi4a.inventoryservice.service;
 
+import com.wesi4a.inventoryservice.dto.InventoryResponse;
 import com.wesi4a.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
@@ -11,7 +16,12 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode){
-        return  inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode){
+        return  inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity()>0)
+                            .build()).toList();
     }
 }
--- order-service/src/main/java/com/wesi4a/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/wesi4a/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.wesi4a.orderservice.service;
 
+import com.wesi4a.orderservice.dto.InventoryResponse;
 import com.wesi4a.orderservice.dto.OrderLineItemsDto;
 import com.wesi4a.orderservice.dto.OrderRequest;
 import com.wesi4a.orderservice.model.Order;
@@ -7,15 +8,20 @@ import com.wesi4a.orderservice.model.OrderLineItems;
 import com.wesi4a.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
 
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -27,7 +33,28 @@ public class OrderService {
                 .toList();
 
         order.setOrderLineItemsList(orderLineItems);
-        orderRepository.save(order);
+
+        List<String> skuCodes =  order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode).toList();
+
+        //call inventory service,and place order if product is in stock
+        InventoryResponse[] inventoryResponseArray =  webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
+                        .retrieve()
+                                .bodyToMono(InventoryResponse[].class)
+                                        .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
+                .allMatch(InventoryResponse::isInStock);
+
+        if(allProductsInStock){
+            orderRepository.save(order);
+        }else{
+            throw new IllegalArgumentException("Product is not in stock,please try again later");
+        }
+
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
